fa51593f2801449be1359dd84b12087a71ef8a82
Andreastak <104064121+Andreastak86@users.noreply.github.com>
2022-10-04 14:58:28 +0200

    Making changes

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,61 +1,106 @@
 import React from "react";
 import "./portfolio.css";
+import IMG1 from "../../assets/drum-kit.jpg";
+import IMG2 from "../../assets/collatz.jpg";
+import IMG3 from "../../assets/to-do.jpg";
+import IMG4 from "../../assets/Cta-oppgave.jpg";
 
 const Portfolio = () => {
   return (
     <section id='portfolio'>
       <h5>Dette har jeg jobbet med den siste tiden</h5>
       <h2>Min Portefølje</h2>
+
       <div className='container portfolio__container'>
         <article className='portfolio__item'>
           <div className='portfolio__item-image'>
-            <h3>Proskjekt-navn</h3>
-            <a
-              href='https://github.com/Andreastak86/Drum-kit.2.0'
-              className='btn'
-              alt='Drum-kit project'
-            >
-              Drum-Kit 2.0
-            </a>
-            <a
-              href='https://andreastak86.github.io/Drum-kit.2.0/'
-              className='btn btn-primary'
-              target='_blank'
-              rel='noreferrer'
-            >
-              Live Demo
-            </a>
-            <a
-              href='https://github.com/Andreastak86/Collatz-Oppgave'
-              className='btn'
-              alt='Collatz-task'
-            >
-              Collatz Formel
-            </a>
-            <a
-              href='https://andreastak86.github.io/Collatz-Oppgave'
-              className='btn btn-primary'
-              target='_blank'
-              rel='noreferrer'
-            >
-              Live Demo
-            </a>
-            <a
-              href='https://github.com/Andreastak86/To-Do-List-2808'
-              className='btn'
-              alt='To-do-List'
-            >
-              To-Do List
-            </a>
-            <a
-              href='https://andreastak86.github.io/To-Do-List-2808/'
-              className='btn btn-primary'
-              target='_blank'
-              rel='noreferrer'
-            >
-              Live Demo
-            </a>
+            <img src={IMG1} alt='project-pictures' />
           </div>
+          <h3>Drum-Kit</h3>
+          <a
+            href='https://github.com/Andreastak86/Drum-kit.2.0'
+            className='btn'
+            alt='Drum-kit project'
+          >
+            Drum-Kit
+          </a>
+          <a
+            href='https://andreastak86.github.io/Drum-kit.2.0/'
+            className='btn btn-primary'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Live Demo
+          </a>
+        </article>
+      </div>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG2} alt='project-pictures' />
+          </div>
+          <a
+            href='https://github.com/Andreastak86/Collatz-Oppgave'
+            className='btn'
+            alt='Collatz-task'
+          >
+            Collatz Formel
+          </a>
+          <a
+            href='https://andreastak86.github.io/Collatz-Oppgave'
+            className='btn btn-primary'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Live Demo
+          </a>
+        </article>
+      </div>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG3} alt='project-pictures' />
+          </div>
+          <a
+            href='https://github.com/Andreastak86/To-Do-List-2808'
+            className='btn'
+            alt='To-do-List'
+          >
+            To-Do List
+          </a>
+          <a
+            href='https://andreastak86.github.io/To-Do-List-2808/'
+            className='btn btn-primary'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Live Demo
+          </a>
+        </article>
+      </div>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG4} alt='project-pictures' />
+          </div>
+          <a
+            href='https://github.com/Andreastak86/Oppgave-planleggingsuke'
+            className='btn'
+            alt='CTA-Oppgave'
+          >
+            ClickToAction
+          </a>
+          <a
+            href='https://andreastak86.github.io/Oppgave-planleggingsuke/'
+            className='btn btn-primary'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Live Demo
+          </a>
         </article>
       </div>
     </section>
